565c9429f404345ee5eb3af973261abaf2466916
ItalyToast <ItalyToast@gmail.com>
2017-07-14 17:49:23 +0200

    QuoteModule + Ignore bot messages

---
+++ Discord Bot/DiscordExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Net.Providers.WS4Net;
+
+namespace Discord_Bot
+{
+    static class DiscordExtensions
+    {
+        public static SocketTextChannel GetLogChannel(this SocketGuild guild)
+        {
+            return guild.Channels.OfType<SocketTextChannel>().First(p => p.Name == "logs");
+        }
+    }
+}
---
+++ Discord Bot/QuoteModule.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    public class QuoteModule : ModuleBase
+    {
+        public QuoteModule()
+        {
+        }
+
+        [Command("quote")]
+        [Summary("Quote a player")]
+        public async Task Quote(string user, [Remainder]string quote)
+        {
+            if (quote.Length < 5)
+            {
+                await Context.Channel.SendMessageAsync("quote to short: " + quote);
+                return;
+            }
+            if (Context.Message.MentionedUserIds.Count == 0)
+            {
+                await Context.Channel.SendMessageAsync("No user mentioned");
+                return;
+            }
+            if (Context.Message.MentionedUserIds.Count >= 2)
+            {
+                await Context.Channel.SendMessageAsync("Only one user may be mentioned");
+                return;
+            }
+
+            string line = WebUtility.HtmlEncode(user) + "#" + WebUtility.HtmlEncode(quote);
+            
+            File.AppendAllText("quotes.txt", line);
+            
+            await Context.Channel.SendMessageAsync("Quote added: " + quote);
+        }
+    }
+}
